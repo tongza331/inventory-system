4976ca521dd1cd73e7ea5a0f3949a86ec8c3ae9a
tongza331 <pst.tongxa@gmail.com>
2021-12-31 19:49:26 +0700

    add dashboard and table

--- inventoryproject/inventoryproject/settings.py
+++ inventoryproject/inventoryproject/settings.py
@@ -118,6 +118,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATTICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
+STATIC_ROOT = (BASE_DIR/"asert/")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
