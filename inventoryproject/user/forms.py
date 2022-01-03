e0a898c0c58f15d5aa10a69ea7a6668c62c3258b
tongza331 <pst.tongxa@gmail.com>
2022-01-03 23:39:07 +0700

    update profile

--- inventoryproject/user/forms.py
+++ inventoryproject/user/forms.py
@@ -1,6 +1,10 @@
 from django import forms
+from .models import Profile
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LogoutView
+from django.db.models import fields
+
 
 class CreateUserForm(UserCreationForm):
     email = forms.EmailField()
@@ -8,3 +12,13 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email','password1','password2']
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username','email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['phone','address','image']
