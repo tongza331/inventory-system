1e24305fbe6b8c3287b4d4a49293b528728097a7
tongza331 <pst.tongxa@gmail.com>
2022-01-03 12:55:13 +0700

    add profile page

--- inventoryproject/inventoryproject/settings.py
+++ inventoryproject/inventoryproject/settings.py
@@ -128,6 +128,10 @@ STATTICFILES_DIRS = [
 
 STATIC_ROOT = (BASE_DIR/"asert/")
 
+MEDIA_ROOT = (BASE_DIR / 'media')
+
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
