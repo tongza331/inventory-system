e276ae81f7c07382b04fee63808abace729856ca
tongza331 <pst.tongxa@gmail.com>
2022-01-06 00:03:17 +0700

    staff make request and fix admin page bug

--- inventoryproject/dashboard/forms.py
+++ inventoryproject/dashboard/forms.py
@@ -1,9 +1,14 @@
 from django import forms
 from django.contrib.auth import models
 from django.forms import fields
-from .models import Product
+from .models import Order, Product
 
 class ProductFrom(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['name','category','quantity']
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ['product', 'order_quantity']
--- inventoryproject/dashboard/models.py
+++ inventoryproject/dashboard/models.py
@@ -21,7 +21,7 @@ class Product(models.Model):
 
 class Order(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
-    staff = models.ForeignKey(User, models.CASCADE, null=True)
+    staff = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     order_quantity = models.PositiveBigIntegerField(null=True)
     date = models.DateTimeField(auto_now_add=True)
 
